7f07a4642d6953a10104d3538d86efc837653e5e
Filip Topic <filip.topic3@gmail.com>
2025-05-25 19:54:19 +0200

    v1

---
+++ src/filter.c
@@ -0,0 +1,38 @@
+/*
+ * (C) 2021 Sascha Hunold
+ */
+
+#include <math.h>
+#include "filter.h"
+#include <omp.h>
+
+
+const int fwidth2  = 1;
+const int fheight2 = 1;
+
+
+void apply_filter(png_bytep *row_pointers, png_bytep *buf, int width, int height, double filter[3][3], int rounds) {    
+    
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            for(int r=0; r<rounds; r++) {        
+                for(int i=fwidth2; i<width - fwidth2; i++) {        
+                    for(int j=fheight2; j<height - fheight2; j++) {        
+                        filter_on_pixel(row_pointers, buf, i, j, filter);
+                    }
+                }
+
+                // swap buffer with image
+                png_bytep *tmp = buf;
+                buf = row_pointers;
+                row_pointers = tmp;
+            }
+        }
+        #pragma omp taskwait
+    }
+    
+    
+}
+
